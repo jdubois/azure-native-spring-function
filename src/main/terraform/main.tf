a596d1972a3c5a3a0d68bda105b2124813fea7d6
Julien Dubois <julien.dubois@gmail.com>
2020-05-30 22:38:36 +0200

    Add Terraform plan

---
+++ src/main/terraform/main.tf
@@ -0,0 +1,38 @@
+provider "azurerm" {
+  version = "~> 2.1"
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "azure-native-spring-function"
+  location = "westeurope"
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "nativespringfunction"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "main" {
+  name                = "azure-native-spring-function-service-plan"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "main" {
+  name                       = "azure-native-spring-function"
+  location                   = azurerm_resource_group.main.location
+  resource_group_name        = azurerm_resource_group.main.name
+  app_service_plan_id        = azurerm_app_service_plan.main.id
+  storage_account_name       = azurerm_storage_account.main.name
+  storage_account_access_key = azurerm_storage_account.main.primary_access_key
+}
