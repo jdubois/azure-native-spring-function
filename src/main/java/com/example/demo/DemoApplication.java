34697145ba740da047fa9f213853798d38b1f7fb
Julien Dubois <julien.dubois@gmail.com>
2020-06-02 16:59:52 +0200

    Set up port programmatically

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,12 +10,11 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(DemoApplication.class);
-/*		String functionServerPort = System.getenv("FUNCTIONS_HTTPWORKER_PORT");
+		String functionServerPort = System.getenv("FUNCTIONS_HTTPWORKER_PORT");
 		if (functionServerPort != null) {
 			app.setDefaultProperties(Collections
 					.singletonMap("server.port", functionServerPort));
-		}*/
+		}
 		app.run(args);
 	}
-
 }
