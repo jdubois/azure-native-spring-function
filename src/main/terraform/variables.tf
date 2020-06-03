e2ff70c6625e11d1fcb0433e1b516cabb45539fa
Julien Dubois <julien.dubois@gmail.com>
2020-06-03 15:36:43 +0200

    Add Terraform variables file

---
+++ src/main/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "AZ_LOCATION" {
+  description = "The Azure location where all resources in this example should be created"
+  default     = "westeurope"
+}
+
+variable "AZ_RESOURCE_GROUP" {
+  description = "The resource group"
+}
+
+variable "AZ_FUNCTION_NAME_APP" {
+  description = "The name of the application running functions"
+}
+
+variable "AZ_STORAGE_NAME" {
+  description = "The name of the storage account"
+}
