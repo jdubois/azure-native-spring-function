a5395356ec908d2213ea7aa7044d51d3f914aef1
Julien Dubois <julien.dubois@gmail.com>
2020-12-10 10:28:34 +0100

    Add a compress script using UPX

---
+++ compress.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+ARTIFACT=spring-native-image
+MAINCLASS=com.example.demo.DemoApplication
+VERSION=0.0.1-SNAPSHOT
+UPX_VERSION=3.96
+UPX=https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo "Install UPX"
+wget https://github.com/upx/upx/releases/download/v$UPX_VERSION/upx-$UPX_VERSION-amd64_linux.tar.xz
+tar -xvf upx-$UPX_VERSION-amd64_linux.tar.xz
+
+echo "Compressing $ARTIFACT with UPX"
+./upx-$UPX_VERSION-amd64_linux/upx --best target/function/spring-native-image
