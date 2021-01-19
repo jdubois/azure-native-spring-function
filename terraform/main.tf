0ba83820d270391f8cfdd61e41b791a9c7a06396
Julien Dubois <julien.dubois@gmail.com>
2021-01-19 14:07:37 +0100

    Switch to standard tier for blob storage, as premium isn't supported

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_storage_account" "main" {
   name                     = var.AZ_STORAGE_NAME
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
-  account_tier             = "Premium"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
