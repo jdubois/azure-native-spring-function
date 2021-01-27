3f9c7445436d86b2f2e27bb2d95ddee18fae9da6
Julien Dubois <julien.dubois@gmail.com>
2021-01-27 16:51:41 +0100

    Remove the need for Jackson as it doesn't work well with GraalVM

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,12 @@
 package com.example.demo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import java.util.Collections;
 
 @SpringBootApplication(proxyBeanMethods = false)
 public class DemoApplication {
-
-	@Bean
-	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(DemoApplication.class);
--- src/main/java/com/example/demo/HelloFunction.java
+++ src/main/java/com/example/demo/HelloFunction.java
@@ -2,15 +2,13 @@ package com.example.demo;
 
 import java.util.function.Function;
 
-import com.example.demo.model.Greeting;
-import com.example.demo.model.User;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HelloFunction implements Function<User, Greeting> {
+public class HelloFunction implements Function<String, String> {
 
     @Override
-    public Greeting apply(User user) {
-        return new Greeting("Hello from GraalVM, " + user.getName() + "!\n");
+    public String apply(String user) {
+        return "Hello from GraalVM, " + user + "!\n";
     }
 }
