da1ffbe6fa479cb92d77dcd144e8b93bda5c2b55
Julien Dubois <julien.dubois@gmail.com>
2021-03-15 16:03:14 +0100

    Force reactive build

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,15 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(HelloFunction.class, args);
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+        app.setWebApplicationType(WebApplicationType.REACTIVE);
+        app.run(args);
     }
 }
