b40ff34267a3d49359c5cf11c332665e053ba8b1
Julien Dubois <julien.dubois@gmail.com>
2022-04-04 20:03:17 +0200

    Fix function

--- src/main/java/com/example/HelloFunction.java
+++ src/main/java/com/example/HelloFunction.java
@@ -9,7 +9,7 @@ import java.util.function.Function;
 @Component
 public class HelloFunction implements Function<Mono<String>, Mono<Greeting>> {
 
-    public Mono<Greeting> apply(Mono<String> name) {
-        return name.map(user -> new Greeting("Hello, " + name + "!\n"));
+    public Mono<Greeting> apply(Mono<String> mono) {
+        return mono.map(name -> new Greeting("Hello, " + name + "!\n"));
     }
 }
